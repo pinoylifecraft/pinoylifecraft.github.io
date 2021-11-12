f28d5880d9057e1bcfba1669b37e36f2a8c174e6
Pinoy LifeCraft <pinoylifecraft@gmail.com>
2021-11-12 21:36:37 +0800

    Add files via upload

--- gameinfo.js
+++ gameinfo.js
@@ -4,12 +4,20 @@ var mobilenavMenuClose = document.getElementById("mobilenavMenuClose")
 var navList = document.getElementById("navList")
 var mobilenavLogo = document.getElementById("mobilenavLogo")
 var mobilenavMenuBackground = document.getElementById("mobilenavMenuBackground")
+var viewerBG1 = document.getElementsByClassName("viewerBG")[0]
 var gameinfoNavButtonsFeatures = document.getElementsByClassName("gameinfoNavButtons")[0]
 var gameinfoNavButtonsMaps = document.getElementsByClassName("gameinfoNavButtons")[1]
 var gameinfoNavButtonsFaqs = document.getElementsByClassName("gameinfoNavButtons")[2]
 var gameinfoNavButtonsStaff = document.getElementsByClassName("gameinfoNavButtons")[3]
+var gameinfoNavButtonsFeatures2 = document.getElementsByClassName("gameinfoNavButtons")[4]
+var gameinfoNavButtonsMaps2 = document.getElementsByClassName("gameinfoNavButtons")[5]
+var gameinfoNavButtonsFaqs2 = document.getElementsByClassName("gameinfoNavButtons")[6]
+var gameinfoNavButtonsStaff2 = document.getElementsByClassName("gameinfoNavButtons")[7]
 var gameinfoIframe = document.getElementById("gameinfoIframe")
-var loadingScreenContainer = document.getElementById("loadingScreenContainer")
+var gameinfoNavButtonsIcon1 = document.querySelectorAll(".gameinfoNavButtonsIcon1")
+var gameinfoNavButtonsIcon2 = document.querySelectorAll(".gameinfoNavButtonsIcon2")
+var gameinfoNavButtonsIcon3 = document.querySelectorAll(".gameinfoNavButtonsIcon3")
+var gameinfoNavButtonsIcon4 = document.querySelectorAll(".gameinfoNavButtonsIcon4")
 
 // PRE LOADER or SPLASH SCREEN
 window.addEventListener("load", function () {
@@ -38,12 +46,32 @@ function mobilenavMenuFadein () {
     mobilenavMenuBackground.style.display = "none"
 }
 
+// TRAILER (PLC SERVER)
+function trailerVideoOpen () {
+    aboutTrailerVideo.setAttribute("src", "https://www.youtube.com/embed/ac5s6a5ZbWM?autoplay=1")
+    viewerBG1.style.display = "block"
+}
+function trailerVideoClose () {
+    aboutTrailerVideo.setAttribute("src", "https://www.youtube.com/embed/ac5s6a5ZbWM?autoplay=0")
+    viewerBG1.style.display = "none"
+}
+
 // GAMEINFO NAVIGATIONS
 function gameinfoButtonsFeatures () {
     gameinfoNavButtonsFeatures.setAttribute("class", "gameinfoNavButtons gameinfoNavButtonsActive")
     gameinfoNavButtonsMaps.setAttribute("class", "gameinfoNavButtons")
     gameinfoNavButtonsFaqs.setAttribute("class", "gameinfoNavButtons")
     gameinfoNavButtonsStaff.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsFeatures2.setAttribute("class", "gameinfoNavButtons gameinfoNavButtonsActive")
+    gameinfoNavButtonsMaps2.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsFaqs2.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsStaff2.setAttribute("class", "gameinfoNavButtons")
+    for (let a = 0; a < gameinfoNavButtonsIcon1.length; a++) {
+        gameinfoNavButtonsIcon1.setAttribute("fill", "#1d73b9")
+        gameinfoNavButtonsIcon2.setAttribute("fill", "#696969")
+        gameinfoNavButtonsIcon3.setAttribute("fill", "#696969")
+        gameinfoNavButtonsIcon4.setAttribute("fill", "#696969")
+    }
     gameinfoIframe.src = "features.html"
 }
 function gameinfoButtonsMaps () {
@@ -51,6 +79,16 @@ function gameinfoButtonsMaps () {
     gameinfoNavButtonsMaps.setAttribute("class", "gameinfoNavButtons gameinfoNavButtonsActive")
     gameinfoNavButtonsFaqs.setAttribute("class", "gameinfoNavButtons")
     gameinfoNavButtonsStaff.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsFeatures2.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsMaps2.setAttribute("class", "gameinfoNavButtons gameinfoNavButtonsActive")
+    gameinfoNavButtonsFaqs2.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsStaff2.setAttribute("class", "gameinfoNavButtons")
+    for (let b = 0; b < gameinfoNavButtonsIcon2.length; b++) {
+        gameinfoNavButtonsIcon1.setAttribute("fill", "#696969")
+        gameinfoNavButtonsIcon2.setAttribute("fill", "#1d73b9")
+        gameinfoNavButtonsIcon3.setAttribute("fill", "#696969")
+        gameinfoNavButtonsIcon4.setAttribute("fill", "#696969")
+    }
     gameinfoIframe.src = "maps.html"
 }
 function gameinfoButtonsFaqs () {
@@ -58,6 +96,16 @@ function gameinfoButtonsFaqs () {
     gameinfoNavButtonsMaps.setAttribute("class", "gameinfoNavButtons")
     gameinfoNavButtonsFaqs.setAttribute("class", "gameinfoNavButtons gameinfoNavButtonsActive")
     gameinfoNavButtonsStaff.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsFeatures2.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsMaps2.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsFaqs2.setAttribute("class", "gameinfoNavButtons gameinfoNavButtonsActive")
+    gameinfoNavButtonsStaff2.setAttribute("class", "gameinfoNavButtons")
+    for (let c = 0; c < gameinfoNavButtonsIcon3.length; c++) {
+        gameinfoNavButtonsIcon1.setAttribute("fill", "#696969")
+        gameinfoNavButtonsIcon2.setAttribute("fill", "#696969")
+        gameinfoNavButtonsIcon3.setAttribute("fill", "#1d73b9")
+        gameinfoNavButtonsIcon4.setAttribute("fill", "#696969")
+    }
     gameinfoIframe.setAttribute("src", "faq.html")
 }
 function gameinfoButtonsStaff () {
@@ -65,6 +113,16 @@ function gameinfoButtonsStaff () {
     gameinfoNavButtonsMaps.setAttribute("class", "gameinfoNavButtons")
     gameinfoNavButtonsFaqs.setAttribute("class", "gameinfoNavButtons")
     gameinfoNavButtonsStaff.setAttribute("class", "gameinfoNavButtons gameinfoNavButtonsActive")
+    gameinfoNavButtonsFeatures2.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsMaps2.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsFaqs2.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsStaff2.setAttribute("class", "gameinfoNavButtons gameinfoNavButtonsActive")
+    for (let d = 0; d < gameinfoNavButtonsIcon4.length; d++) {
+        gameinfoNavButtonsIcon1.setAttribute("fill", "#696969")
+        gameinfoNavButtonsIcon2.setAttribute("fill", "#696969")
+        gameinfoNavButtonsIcon3.setAttribute("fill", "#696969")
+        gameinfoNavButtonsIcon4.setAttribute("fill", "#1d73b9")
+    }
     gameinfoIframe.src = "staff.html"
 }
 function gameinfoButtonsRules () {
@@ -72,10 +130,13 @@ function gameinfoButtonsRules () {
     gameinfoNavButtonsMaps.setAttribute("class", "gameinfoNavButtons")
     gameinfoNavButtonsFaqs.setAttribute("class", "gameinfoNavButtons")
     gameinfoNavButtonsStaff.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsFeatures2.setAttribute("class", "gameinfoNavButtons gameinfoNavButtonsInactive")
+    gameinfoNavButtonsMaps2.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsFaqs2.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsStaff2.setAttribute("class", "gameinfoNavButtons")
+    gameinfoNavButtonsIcon1.setAttribute("fill", "#696969")
+    gameinfoNavButtonsIcon2.setAttribute("fill", "#696969")
+    gameinfoNavButtonsIcon3.setAttribute("fill", "#696969")
+    gameinfoNavButtonsIcon4.setAttribute("fill", "#696969")
     gameinfoIframe.src = "rules.html"
 }
-
-// GAMEINFO IFRAME LOADER
-window.addEventListener("load", function () {
-    loadingScreenContainer.style.animation = "loadingScreen .4s forwards cubic-bezier(0.4, 0, 1, 1)"
-})
---
+++ gameinfobranches.js
@@ -0,0 +1,6 @@
+var loadingScreenContainer = document.getElementById("loadingScreenContainer")
+
+// GAMEINFO IFRAME LOADER
+window.addEventListener("load", function () {
+    loadingScreenContainer.style.animation = "loadingScreen .4s forwards cubic-bezier(0.4, 0, 1, 1)"
+})
