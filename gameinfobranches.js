dd573e949f0c57d982bd0d52265856cce5a315bd
Pinoy LifeCraft <pinoylifecraft@gmail.com>
2021-11-17 15:34:51 +0800

    Delete gameinfobranches.js

--- gameinfobranches.js
+++
@@ -1,6 +0,0 @@
-var loadingScreenContainer = document.getElementById("loadingScreenContainer")
-
-// GAMEINFO IFRAME LOADER
-window.addEventListener("load", function () {
-    loadingScreenContainer.style.animation = "loadingScreen .4s forwards cubic-bezier(0.4, 0, 1, 1)"
-})
